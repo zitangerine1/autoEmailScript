22d79d88561f859cb262e8d223db885e1734ede5
zitangerine <hs190182@sji-international.com.sg>
2023-07-03 03:10:54 +0800

    minor update

--- readUtils.py
+++ readUtils.py
@@ -1,5 +1,7 @@
 import csv
 
+# TODO Implement skipping empty rows
+
 # Returns an array of tuples of (String, String), which will contain the Company name and their email
 def getSponsorList(filepath):
     emailList = []
@@ -10,8 +12,11 @@ def getSponsorList(filepath):
             if line_count == 0:
                 # ignore headers
                 line_count += 1
+            # If the row is empty, skip it
+            # Or accept a placeholder value - if it's there, skip it
+            # Or check if there is an "@" in the string.
             else:
-                emailList.append((row[0],row[1]))
+                emailList.append((row[0], row[1]))
                 line_count += 1
         print(f'Processed {line_count} addresses.')
     return emailList
