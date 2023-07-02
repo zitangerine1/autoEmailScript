4b7f91e334b697d2c654e44dbf2867a510b755c6
zitangerine <hs190182@sji-international.com.sg>
2023-07-02 21:33:27 +0800

    create gitignore

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ import ssl
 from email.message import EmailMessage
 from readUtils import getSponsorList
 
-# TODO - Hide the email password, ideally by using environment variables.
-# TODO - Integrate get from spreadsheet and send email.
-# TODO - Attach PDF prospectus to email.
+# TODO - HTML emails.
 
-#emailSender = 'sponsor.cloudhacks@gmail.com'
-#emailPassword = 'wnkvooxnmavgofav'
-#emailReceiver = 'zitangr@gmail.com'
+emailSender = 'sponsor.cloudhacks@gmail.com'
+emailPassword = 'wnkvooxnmavgofav'
+emailReceiver = 'zitangr@gmail.com'
 
-sponsorList = getSponsorList("assets\CloudHacks Dummy Test Data.csv")
+sponsorList = getSponsorList("./assets/CloudHacks Dummy Test Data.csv")
 
 for sponsor in sponsorList:
     subject = 'Test Email'
@@ -23,14 +21,11 @@ for sponsor in sponsorList:
     msg['Subject'] = subject
     msg.set_content(body)
 
-    # sponsor[0] is sponsor name
-
     with open('./assets/cloudhacks_prospectus.pdf', 'rb') as f:
         file_data = f.read()
         file_name = f.name
 
     msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename='cloudhacks_prospectus.pdf')
-
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
