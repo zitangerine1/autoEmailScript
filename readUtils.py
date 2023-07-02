b56a96b9c49c7dd79466e373af305910bde44382
branhaya4 <83571677+branhaya4@users.noreply.github.com>
2023-07-02 21:21:01 +0800

    Update readUtils.py

--- readUtils.py
+++ readUtils.py
@@ -1,3 +1,17 @@
+import csv
+
 # Returns an array of tuples of (String, String), which will contain the Company name and their email
-def getSponsorList():
-    pass
+def getSponsorList(filepath):
+    emailList = []
+    with open(filepath) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                # ignore headers
+                line_count += 1
+            else:
+                emailList.append((row[0],row[1]))
+                line_count += 1
+        print(f'Processed {line_count} addresses.')
+    return emailList
